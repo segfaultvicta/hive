0de63bdc1319a54794ce8b4e72becb381a756804
Jonathan C. Cantwell <jon.c.cantwell@gmail.com>
2020-08-24 23:08:20 -0400

    try this

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,14 @@
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background: #B34E7E;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<main>
+	<slot></slot>
+</main>
